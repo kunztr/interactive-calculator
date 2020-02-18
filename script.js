c4e52f192b7142937aab364f856a0bb55ba8b206
Erik on Voyager2 <ewfalor@gmail.com>
2020-02-17 19:50:43 -0700

    CS2610 Assignment 3 starter code

---
+++ script.js
@@ -0,0 +1,3 @@
+/* TODO: Build up the HTML document by using JavaScript DOM manipulation functions.
+ * Do not rely on changes you may have made to index.html because the grader won't use that file.
+ */
